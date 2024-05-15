f9d454842e3fd4df84a3912a1471b63f1e395c13
pcmanoss <169086577+pcmanoss@users.noreply.github.com>
2024-05-15 16:46:11 +0300

    Add files via upload

---
+++ panel/credentials.php
@@ -0,0 +1,4 @@
+<?php
+$name = "Neon"; 
+$OwnerId = "cOy8ZsP9dq";
+$SellerKey = "https://keyauth.win/api/seller/?sellerkey=f849a8a71122a71ce0e6e68edae86c78&type=add&expiry=1&mask=******-******-******-******-******-******&level=1&amount=1&format=text";
